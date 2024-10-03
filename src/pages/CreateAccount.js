2800a37a81b2fc4a0ad73d3980fc6b418c91c789
Lucas França <lucasfreitas_c5@hotmail.com>
2024-10-03 07:34:09 -0300

    save changes

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -3,7 +3,17 @@ import Layout from "../components/Layout";
 const CreateAccount = () => {
     return (
         <Layout showFooter={false}>
-            <p>CRIAR CONTA</p>
+            <form>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </Layout>
 
     )
