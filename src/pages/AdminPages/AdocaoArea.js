93f605052efbb71d739a5e6002c7ef88102ccaf0
Lucas França <lucasfreitas_c5@hotmail.com>
2025-01-10 07:59:42 -0300

    salvando

---
+++ src/pages/AdminPages/AdocaoArea.js
@@ -0,0 +1,15 @@
+import AdminArea from "./AdminArea"
+
+
+const AdocaoArea = () => {
+
+    return (
+        <AdminArea>
+            <h1>Gerenciamento de Adoções</h1>
+
+        </AdminArea>
+    )
+}
+
+
+export default AdocaoArea 
