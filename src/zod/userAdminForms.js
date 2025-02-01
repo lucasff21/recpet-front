84a916b3a05c157926b33eaaeefc829385a2cfc0
Milena <elenprojects7@gmail.com>
2025-02-01 11:01:35 -0300

    feat: regras para criar usuário pelo admin

---
+++ src/zod/userAdminForms.js
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const schemaForm = z.object({
+    personalData: z.object({
+        fullName: z.string().nonempty("Nome completo é obrigatório"),
+        email: z.string().nonempty("Email é obrigatório").email("Email inválido."),
+        role: z.string().nonempty("Tipo é obrigatório"),
+        password: z.string().nonempty("Senha é obrigatório")
+            .min(8, 'A senha deve ter pelo menos 8 caracteres')
+            .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
+            .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
+            .regex(/[\W_]/, 'A senha deve conter pelo menos um símbolo'),
+    })
+});
