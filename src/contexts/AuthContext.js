2d40aa70faf8bd37532352d0688ba7089024bc32
Milena <elenprojects7@gmail.com>
2025-02-01 10:58:37 -0300

    feat: verifica se token ta expirado e retira do localstorage, adiciona informação de loading

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -8,9 +8,18 @@ export const AuthProvider = ({ children }) => {
         return localStorage.getItem('Token_RecSys'); // Inicializa com o token do localStorage
     });
     const [role, setRole] = useState(null);
-    const [userEmail, setUserEmail] = useState(null)
+    const [userEmail, setUserEmail] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const isAuthenticated = !!authToken; 
+    const isTokenExpired = (token) => {
+        try {
+            const decodedToken = jwtDecode(token);
+            let expired = decodedToken.exp * 1000 < Date.now();
+            return expired;
+        } catch { return true; }
+    };
+
+    const isAuthenticated =  !isTokenExpired(authToken) && !!authToken;
 
     const login = (token) => {
         setAuthToken(token);
@@ -27,32 +36,28 @@ export const AuthProvider = ({ children }) => {
     const tokenDecode = (token) => {
         try {
             const decodedToken = jwtDecode(token);
-            const userRole = decodedToken.ROLE || null; 
+            const userRole = decodedToken.ROLE || null;
+            const userEmailToken = decodedToken.sub || null;
+            setUserEmail(userEmailToken)
             setRole(userRole); 
         } catch (error) {
             console.error("Erro ao decodificar o token:", error);
         }
     };
 
-    const userEmailToken = (token) => {
-        try {
-            const decodedToken = jwtDecode(token);
-            const userEmailToken = decodedToken.sub || null;
-            setUserEmail(userEmailToken)
-        } catch (error) {
-            console.error("Erro ao decodificar o token:", error);
-        }
-    }
-
     useEffect(() => {
         if (authToken) {
-            tokenDecode(authToken); 
-            userEmailToken(authToken);
+            if (!isTokenExpired(authToken)) {
+                tokenDecode(authToken);
+            } else {
+                logout();
+            }
         }
+        setLoading(false);
     }, [authToken]); 
 
     return (
-        <AuthContext.Provider value={{ authToken, isAuthenticated, login, logout, role, userEmail }}>
+        <AuthContext.Provider value={{ authToken, isAuthenticated, login, logout, role, userEmail, loading }}>
             {children}
         </AuthContext.Provider>
     );
