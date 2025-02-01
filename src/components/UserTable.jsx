4a49f428d2654f24662ea77d188dd5a704c4cbc2
Milena <elenprojects7@gmail.com>
2025-02-01 11:02:31 -0300

    feat: criar tabela de gerenciamento de usuarios

---
+++ src/components/UserTable.jsx
@@ -0,0 +1,149 @@
+import React, {useEffect, useState} from "react";
+import {getUsers} from "../services/ApiUser";
+import {toast} from "react-toastify";
+import {Link} from "react-router-dom";
+import {deleteUser} from "../services/ApiAdmin";
+import ConfirmModal from "./ConfirmModal";
+
+const UserTable = () => {
+    const [users, setUsers] = useState([])
+    const [filters, setFilters] = useState({ query: '', role: '', sortByDate: 'desc' });
+    const [total, setTotal] = useState([]);
+    const [modal, setModal] = useState({ isOpen: false, userId: null });
+
+    const handleFilterChange = (e) => {
+        const { name, value } = e.target;
+        setFilters({ ...filters, [name]: value });
+    };
+
+    const filteredUsers = users
+        .filter((user) => {
+            const queryMatch =
+                user.nome.toLowerCase().includes(filters.query.toLowerCase()) ||
+                user.email.toLowerCase().includes(filters.query.toLowerCase());
+            const roleMatch = filters.role === '' || user['tipoUsuario'] === filters.role;
+            return queryMatch && roleMatch;
+        })
+        .sort((a, b) => {
+            if (filters.sortByDate === 'desc') {
+                return b.criadoEm - a.criadoEm;
+            } else {
+                return a.criadoEm - b.criadoEm;
+            }
+        });
+
+    const handleDelete = (id) => {
+        setModal({ isOpen: true, userId: id });
+    };
+
+    const confirmDelete = () => {
+        deleteUser(modal.userId)
+            .then(() => {
+                setUsers(users.filter((user) => user.id !== modal.userId));
+                toast("Sucesso ao deletar o usuário", { position: 'bottom-right', autoClose: 5000 });
+            })
+            .catch(() => {
+                toast("Erro ao deletar o usuário", { type: 'error', position: 'bottom-right', autoClose: 5000 });
+            });
+        setModal({ isOpen: false, userId: null });
+    };
+
+
+    const handleEdit = (id) => {
+        alert(`Edit user with ID: ${id}`);
+    };
+
+    const tipos = {
+        ADMIN: "ADMINISTRADOR",
+        MODERATOR: "MODERADOR",
+        ADOTANTE: "ADOTANTE"
+    }
+
+    const getUsersFiltered = (filters = {}) => {
+        getUsers(filters)
+            .then((response) => {
+                setTotal(response.data['totalElements']);
+                setUsers(response.data.content);
+            })
+            .catch(() => {
+                toast("Erro ao buscar os usuários", {
+                    type: 'error',
+                    position: 'bottom-right',
+                    autoClose: 5000,
+                });
+            });
+    }
+
+    useEffect(() => {
+        getUsersFiltered()
+    }, []);
+
+    const handleFilteredSearch = () => {
+        getUsersFiltered(filters)
+    }
+
+    return (
+        <div>
+            <h1>Usuários ({total})</h1>
+            <div className="filters">
+                <input
+                    type="text"
+                    name="query"
+                    placeholder="Pesquise por nome or email"
+                    value={filters.query}
+                    onChange={handleFilterChange}
+                />
+                <select name="role" value={filters.role} onChange={handleFilterChange}>
+                    <option value="">Todos</option>
+                    {Object.entries(tipos).map(([key, value]) => (
+                        <option key={key} value={key}> {value} </option>
+                    ))}
+                </select>
+                <select name="sortByDate" value={filters.sortByDate} onChange={handleFilterChange}>
+                    <option value="desc">Mais Recentes</option>
+                    <option value="asc">Mais Antigos</option>
+                </select>
+                <button type="button" className="btn btn-success">
+                    <Link to="../criar" className="list-group-item list-group-item-action">+ Criar usuário</Link>
+                </button>
+                {/* <button type="button" className="btn btn-success" onClick={handleFilteredSearch}>Pesquisar</button>*/}
+            </div>
+            <table>
+                <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Tipo</th>
+                    <th>Ações</th>
+                </tr>
+                </thead>
+                <tbody>
+                {filteredUsers.map((user) => (
+                    <tr key={user.id}>
+                        <td>{user.nome}</td>
+                        <td>{user.email}</td>
+                        <td>{tipos[user['tipoUsuario']]}</td>
+                        <td>
+                            <button className="edit-btn" onClick={() => handleEdit(user.id)}>
+                                ✏️ Editar
+                            </button>
+                            <button className="delete-btn" onClick={() => handleDelete(user.id)}>
+                                🗑️ Excluir
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            {modal.isOpen && (
+                <ConfirmModal
+                    message="Tem certeza que deseja excluir este usuário?"
+                    onConfirm={confirmDelete}
+                    onCancel={() => setModal({ isOpen: false, userId: null })}
+                />
+            )}
+        </div>
+    )
+}
+
+export default UserTable
