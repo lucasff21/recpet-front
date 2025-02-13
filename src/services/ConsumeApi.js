7bbd7a5655f2ebee46a267bd7edec704ab4d9c4f
Lucas França <lucasfreitas_c5@hotmail.com>
2025-02-12 21:02:22 -0300

    ajustes

--- src/services/ConsumeApi.js
+++ src/services/ConsumeApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { BASE_API_URL } from '../helpers/apiRoutes'
 
-const apiUrl = BASE_API_URL.baseUrl
+const apiUrl = BASE_API_URL.deploymentUrl
 
 export const CachorroFindAll = async () => {
     try {
