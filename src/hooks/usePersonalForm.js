ed9e33e24967617968d3ca65a90077992e3844b5
Milena <elenprojects7@gmail.com>
2025-02-22 14:46:43 -0300

    feat: muda redirecionamento ao criar conta

--- src/hooks/usePersonalForm.js
+++ src/hooks/usePersonalForm.js
@@ -84,7 +84,7 @@ export const usePersonalForm = () => {
             let response = await createUser(payload);
 
             if (response.status === 201) {
-                navigate("/login", { state: { userCreated: true } });
+                navigate("/area-cliente", { state: { userCreated: true } });
             }
             showToast('Usuário criado com sucesso')
         } catch (error) {
