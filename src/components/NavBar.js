1b48def3aa05d82117fe66c5f3a02671de5b8635
Lucas França <lucasfreitas_c5@hotmail.com>
2025-03-29 10:17:39 -0300

    nova tela de login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,15 +11,15 @@ const NavBar = () => {
 
   useEffect(() => {
     getUser()
-}, [userEmail]); 
+  }, [userEmail]);
 
   const getUser = async () => {
     console.log('ENTROU')
 
-    if(userEmail && authToken){
+    if (userEmail && authToken) {
       const userGet = await findByUserEmail(userEmail, authToken)
 
-      if(userGet){
+      if (userGet) {
         console.log(userGet.email)
         setUser(userGet)
       }
@@ -28,7 +28,7 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light nav-principal" style={{ backgroundColor: '#f0ac4c' }}>
+      <nav className="navbar navbar-expand-lg navbar-light nav-principal" style={{ backgroundColor: '#FFF' }}>
         <img src={logo} alt="Logo" className="rounded-circle" />
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -37,19 +37,16 @@ const NavBar = () => {
           <div className="navbar-nav">
             <a className="nav-item nav-link" href="/">Home</a>
             <a className="nav-item nav-link" href="#">Quem Somos</a>
-            <a className="nav-item nav-link" href="#">Cadastrar Pet</a>
             <a className="nav-item nav-link" href="#">Contato</a>
           </div>
         </div>
         <div className="collapse navbar-collapse nav-area-cliente2">
           <div className="navbar-nav">
-            <a className="nav-item nav-link" href="/area-cliente">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
-                <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
-              </svg>
-              {user ? (user.name || user.email) : "Área do Cliente"}
-              </a>
+            <button type="button" className="button-customer1" onClick={() => window.location.href = "/area-cliente"}>
+              {user ? (user.name || user.email) : "Entrar"}
+            </button>
           </div>
+
         </div>
       </nav>
 
@@ -67,9 +64,6 @@ const NavBar = () => {
               <a className="nav-link" href="#">Quem Somos</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Cadastrar Pet</a>
-            </li>
-            <li className="nav-item">
               <a className="nav-link" href="#">Contato</a>
             </li>
             <li className="nav-item">
@@ -77,8 +71,8 @@ const NavBar = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
                   <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
                 </svg>
-                {}
-            {user ? (user.name || user.email) : "Área do Cliente"}
+                { }
+                {user ? (user.name || user.email) : "Entrar"}
               </a>
             </li>
           </ul>
