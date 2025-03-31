3b1f6ba0f77f8039860938c6a929e45cfb9ab177
Milena <elenprojects7@gmail.com>
2025-03-30 22:01:15 -0300

    feat(Blog): página base de Blog

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+const Blog = () => {
+  return (
+      <Layout>
+          <div>
+              <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">Blog</h1>
+          </div>
+      </Layout>
+  );
+}
+
+export default Blog;
