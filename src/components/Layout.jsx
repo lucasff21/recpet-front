189768b8706c1460a62bc92ed14eef41a4a256de
Milena <elenprojects7@gmail.com>
2025-03-30 22:06:16 -0300

    feat(Layout): passa a usar tailwind

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,17 @@
 import Footer from './Footer';
 import Header from './Header';
-import '../styles/ComponentLayout.css';
+import {ToastContainer} from "react-toastify";
 
 
 const Layout = ({ children, showNavBar= true, showFooter = true }) => { 
     return (
-        <div className="layout-container">
+        <div className="flex flex-col min-h-screen">
+            <ToastContainer/>
             {showNavBar && <Header />}
             <main>
-                {children}  
+                {children}
             </main>
-            {showFooter && <Footer />} 
+            {showFooter && <Footer />}
         </div>
     );
 };
