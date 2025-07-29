be402477541e7573a340155064c46439a82ed86e
Milena <elenprojects7@gmail.com>
2025-07-29 18:58:20 -0300

    feat: atualizar usuário no context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,31 +1,29 @@
-import { createContext, useState, useEffect } from 'react';
-import { jwtDecode } from 'jwt-decode'; // Importação correta
+import { createContext, useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [authToken, setAuthToken] = useState(() => {
-    return localStorage.getItem('Token_RecSys'); // Inicializa com o token do localStorage
+    return localStorage.getItem('Token_RecSys');
   });
   const [role, setRole] = useState(null);
   const [userEmail, setUserEmail] = useState(null);
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(() => {
-    return JSON.parse(localStorage.getItem('User_RecSys')); // Inicializa com o token do localStorage
+    return JSON.parse(localStorage.getItem('User_RecSys'));
   });
 
   const isTokenExpired = (token) => {
     try {
       const decodedToken = jwtDecode(token);
-      let expired = decodedToken.exp * 1000 < Date.now();
-      return expired;
+      return decodedToken.exp * 1000 < Date.now();
     } catch {
       return true;
     }
   };
 
   const isAuthenticated = !isTokenExpired(authToken) && !!authToken;
-
   const isAdmin = isAuthenticated && role === 'ADMIN';
 
   const login = (data) => {
@@ -42,6 +40,11 @@ export const AuthProvider = ({ children }) => {
     setUser(null);
     localStorage.removeItem('Token_RecSys');
     localStorage.removeItem('User_RecSys');
+  };
+
+  const updateUserContext = (updatedUserData) => {
+    setUser(updatedUserData);
+    localStorage.setItem('User_RecSys', JSON.stringify(updatedUserData));
   };
 
   const tokenDecode = (token) => {
@@ -79,6 +82,7 @@ export const AuthProvider = ({ children }) => {
         loading,
         user,
         isAdmin,
+        updateUserContext,
       }}
     >
       {children}
