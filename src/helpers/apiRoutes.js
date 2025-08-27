a9f7be853db7b9011d38a11f15854460d7db77de
Lucas França <lucasfreitas_c5@hotmail.com>
2025-08-27 00:29:39 -0300

    var

--- src/helpers/apiRoutes.js
+++ src/helpers/apiRoutes.js
@@ -1,12 +1,14 @@
-/*export const URL = {
+export const URL = {
   base: process.env.REACT_APP_BASE_URL,
   api: process.env.REACT_APP_API_URL,
 };
-*/
 
+
+/*
 export const URL = {
   base: 'http://localhost:8080',
 api: 'http://localhost:8080'
 };
 
 
+*/
