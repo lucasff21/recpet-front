b0ed8a015733186a4b2bfb5133ee7bdd141bdf8b
Milena <elenprojects7@gmail.com>
2025-09-14 19:35:07 -0300

    feat: adiciona dias na idade

--- src/utils/pet.js
+++ src/utils/pet.js
@@ -4,17 +4,35 @@ export function calculateAge(birthDateStr) {
 
   let years = today.getFullYear() - birthDate.getFullYear();
   let months = today.getMonth() - birthDate.getMonth();
+  let days = today.getDate() - birthDate.getDate();
+
+  if (days < 0) {
+    months--;
+    const daysInLastMonth = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      0
+    ).getDate();
+    days += daysInLastMonth;
+  }
 
   if (months < 0) {
     years -= 1;
     months += 12;
   }
 
-  if (years > 0 && months > 0) {
-    return `${years} ano${years > 1 ? 's' : ''} e ${months} ano${months > 1 ? 's' : ''}`;
-  } else if (years > 0) {
-    return `${years} ano${years > 1 ? 's' : ''}`;
-  } else {
+  if (years > 0) {
+    const yearText = `${years} ano${years > 1 ? 's' : ''}`;
+    const monthText =
+      months > 0 ? ` e ${months} ${months > 1 ? 'meses' : 'mês'}` : '';
+    return `${yearText}${monthText}`;
+  }
+
+  if (months > 0) {
     return `${months} ${months > 1 ? 'meses' : 'mês'}`;
+  }
+
+  if (days >= 0) {
+    return `${days} dia${days > 1 ? 's' : ''}`;
   }
 }
