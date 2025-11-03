02399e779fe3d558ca4cf60f419ecd4f28e44af3
Milena <elenprojects7@gmail.com>
2025-11-03 00:07:18 -0300

    refactor

--- src/zod/personalForms.js
+++ src/zod/personalForms.js
@@ -4,19 +4,12 @@ import { validateCPF } from '../utils/validateCpf';
 export const schemaForm = z
   .object({
     fullName: z.string().nonempty('Nome completo é obrigatório'),
-    cpf: z
-      .string()
-      //.length(11, 'CPF deve ter 11 dígitos')
-      .refine((value) => validateCPF(value), 'CPF inválido'),
+    cpf: z.string().refine((value) => validateCPF(value), 'CPF inválido'),
     gender: z.string().nonempty('Gênero é obrigatório'),
     birthDate: z.string().nonempty('Data de nascimento é obrigatória'),
     phone: z
       .string({ invalid_type_error: 'Celular deve ser uma string' })
-      .nonempty('Email é obrigatório')
-      .regex(
-        /^\d{10,11}$/,
-        'Celular deve conter apenas números e ter entre 10 e 11 dígitos'
-      ),
+      .nonempty('Telefone é obrigatório'),
     email: z.string().nonempty('Email é obrigatório').email('Email inválido.'),
     confirmEmail: z
       .string()
@@ -41,12 +34,7 @@ export const schemaForm = z
         required_error: 'CEP é obrigatório',
         invalid_type_error: 'CEP deve ser uma string',
       })
-      .regex(
-        /^\d{5}-?\d{3}$/,
-        'CEP inválido, deve estar no formato 00000-000 ou 00000000'
-      )
-      .min(8, 'CEP deve conter pelo menos 8 caracteres')
-      .max(9, 'CEP deve conter no máximo 9 caracteres'),
+      .min(8, 'CEP deve conter pelo 8 caracteres'),
     street: z.string().nonempty('Logradouro é obrigatório'),
     complement: z.string().optional(),
     district: z.string().nonempty('Bairro é obrigatório'),
