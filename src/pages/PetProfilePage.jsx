2a80bdffb52b5e43eda4d819185b42c3d981cb89
Deivid Costa <deivid652015@gmail.com>
2025-11-10 15:30:14 -0300

    fix: atualizar rótulo de 'Raça' para 'Pelagem' na página de perfil do pet

--- src/pages/PetProfilePage.jsx
+++ src/pages/PetProfilePage.jsx
@@ -204,9 +204,9 @@ const PetProfilePage = () => {
                   <div className="grid grid-cols-2 gap-4">
                     <div className="space-y-2">
                       <div>
-                        <p className="text-sm text-gray-500 m-0">Raça</p>
+                        <p className="text-sm text-gray-500 m-0">Pelagem</p>
                         <p className="font-medium text-base md:text-lg">
-                          {selectedPet.raca || 'SRD (Sem Raça Definida)'}
+                          {selectedPet.pelagem|| 'SRD (Sem Raça Definida)'}
                         </p>
                       </div>
                       <div>
