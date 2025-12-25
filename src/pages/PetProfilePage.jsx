0ef26b9ab21ceba03f9eb72ff9fceb270f553968
Milena <elenprojects7@gmail.com>
2025-12-25 15:10:49 -0300

    ajustes

--- src/pages/PetProfilePage.jsx
+++ src/pages/PetProfilePage.jsx
@@ -285,7 +285,7 @@ const PetProfilePage = () => {
                       <div>
                         <p className="text-sm text-gray-500 m-0">Raça</p>
                         <p className="font-medium text-base md:text-lg">
-                          {selectedPet.raca || '—'}
+                          {selectedPet.raca || 'SRD'}
                         </p>
                       </div>
                       <div>
@@ -373,10 +373,10 @@ const PetProfilePage = () => {
                       </div>
                       {selectedPet.observacoesMedicas && (
                         <div className="mt-4">
-                          <h3 className="text-base font-semibold text-gray-700">
+                          <p className="text-sm text-gray-500 m-0">
                             Observações Médicas:
-                          </h3>
-                          <p className="text-gray-800 mt-1">
+                          </p>
+                          <p className="font-medium text-base md:text-lg">
                             {selectedPet.observacoesMedicas}
                           </p>
                         </div>
